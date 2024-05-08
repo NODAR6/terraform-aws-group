f7d181584dc5295a47be06a1c54d0114649a1585
NODAR6 <164959031+NODAR6@users.noreply.github.com>
2024-05-08 05:51:44 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,16 +1,18 @@
+resource "aws_launch_configuration" "example" {
+  name_prefix   = "nodar-"
+  image_id      = var.ami
+  instance_type = var.instance_type
+}
 
-
-
-resource "aws_autoscaling_group" "autoscale" {
-  name                  = "test-autoscaling-group"  
-  availability_zones    = ["us-west-1"]
+resource "aws_autoscaling_group" "instance" {
+  name                  = "nodar"  
+  availability_zones    = ["us-east-1"]
   desired_capacity      = 3
   max_size              = 6
   min_size              = 3
   health_check_type     = "EC2"
-  # termination_policies  = ["OldestInstance"]
-  # vpc_zone_identifier   = ["subnet-12345678"]
 
-
+  launch_configuration = aws_launch_configuration.example.id
   
+
 }
