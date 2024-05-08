81413bb7381df2d63fab6f4665afe28c3d392c39
NODAR6 <164959031+NODAR6@users.noreply.github.com>
2024-05-07 20:59:26 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -2,12 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_launch_template" "template" {
-  name_prefix     = "test"
-  image_id        = "ami-1a2b3c"
-  instance_type   = "t2.micro"
-  security_groups = ["sg-12345678"]
-}
+
 
 resource "aws_autoscaling_group" "autoscale" {
   name                  = "test-autoscaling-group"  
@@ -16,11 +11,10 @@ resource "aws_autoscaling_group" "autoscale" {
   max_size              = 6
   min_size              = 3
   health_check_type     = "EC2"
-  termination_policies  = ["OldestInstance"]
-  vpc_zone_identifier   = ["subnet-12345678"]
+  # termination_policies  = ["OldestInstance"]
+  # vpc_zone_identifier   = ["subnet-12345678"]
+  key_name= "local"
+  instance_type = "t2.micro"
 
-  launch_template {
-    id      = aws_launch_template.template.id
-    version = "$Latest"
-  }
+  
 }
